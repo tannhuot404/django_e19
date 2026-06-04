bfe64ca3e6b5ad8fd5f45d5025937d3f45ec3932
Tannhuot KHOUV <khouvtannhuot@gmail.com>
2026-06-04 20:42:57 +0700

    parent and child layout example

--- django_e19/settings.py
+++ django_e19/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'category',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'django_e19.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
