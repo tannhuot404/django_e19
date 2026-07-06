04874fec069adf57c5e2c7de0dd10a49143b1009
Tannhuot KHOUV <khouvtannhuot@gmail.com>
2026-07-06 20:52:03 +0700

    handle form

--- category/urls.py
+++ category/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'category'
+
 urlpatterns = [
-    path('category/', views.index)
+    path('category/', views.index, name='index'),
+    path('category/create', views.create, name='create')
 ]
--- category/views.py
+++ category/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     return render(request, "category/index.html")
+
+def create(request):
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+
+        if name:
+            print(f"Category Name: {name}")
+            return redirect('category:index')
+
+    return render(request, 'category/create.html')
